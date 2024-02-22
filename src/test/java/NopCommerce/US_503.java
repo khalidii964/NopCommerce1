2c7dfb5d4828ce3e31d2389ea8fd2e8e2e147299
Hamza <gokcehamza2934@gmail.com>
2024-02-23 01:26:15 +0300

    sa

--- src/test/java/NopCommerce/US_503.java
+++ src/test/java/NopCommerce/US_503.java
@@ -1,4 +1,22 @@
 package NopCommerce;
 
+import Pages.noCommercePages;
+import Utilities.ConfigReader;
+import Utilities.UIHelpers;
+import Utilities.WebDriverFactory;
+import org.testng.annotations.BeforeMethod;
+
 public class US_503 {
+
+    UIHelpers uiHelpers =new UIHelpers();
+    noCommercePages noCommercePages=new noCommercePages();
+    @BeforeMethod
+    public void seetUp(){
+        WebDriverFactory.getDriver().get(ConfigReader.getProperty("NopCommerce"));
+    }
+
+
+
+
+
 }
