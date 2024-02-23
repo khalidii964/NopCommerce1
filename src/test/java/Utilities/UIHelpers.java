3b843dc51e4c69cde272d0a18105f72fd4a01787
khudoyarovaaziza <153706206+khudoyarovaaziza@users.noreply.github.com>
2024-02-23 01:55:00 -0500

    almost finished

--- src/test/java/Utilities/UIHelpers.java
+++ src/test/java/Utilities/UIHelpers.java
@@ -1,8 +1,11 @@
 package Utilities;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
@@ -12,6 +15,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class UIHelpers {
+
     public static void waitInSeconds(int sec){
         try{
             Thread.sleep(sec * 1000);
@@ -19,6 +23,9 @@ public class UIHelpers {
             throw new RuntimeException();
         }
     }
+
+
+
     public void screenShot(WebDriver driver)  {
         TakesScreenshot screenshot = (TakesScreenshot) driver;
         LocalDateTime timeOfBug = LocalDateTime.now();
@@ -28,25 +35,42 @@ public class UIHelpers {
         File fullPage = new File(filePath);
         File tempFile =  screenshot.getScreenshotAs(OutputType.FILE);
         try {
+            // Which Dependency are we use for FileUtils in Java Selenium
             FileUtils.copyFile(tempFile,fullPage);
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
-    public static void scrollToElementJsExecutor(WebElement element) {
+
+    public void scrollToElementJsExecutor(WebElement element) {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) WebDriverFactory.getDriver();
         jsExecutor.executeScript("arguments[0].scrollIntoView(true);",element );
     }
-    //        uiHelpers.scrollToElementJsExecutor(exercisePage.createAccountSignUpButton);
-    public static void clickOnElementJsExecutor(WebElement element){
+
+    public void clickOnElementJsExecutor(WebElement element){
         JavascriptExecutor jsExecutor = (JavascriptExecutor) WebDriverFactory.getDriver();
         jsExecutor.executeScript("arguments[0].click();", element);
     }
-    //        UIHelpers.clickOnElementJsExecutor(DemoMRS.OpenMRS2DemoButton);
+
+
+    public WebElement waitForVisibility(WebElement element, int timeout){
+        WebDriverWait wait = new WebDriverWait(WebDriverFactory.getDriver(), Duration.ofSeconds(timeout));
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
     public WebElement waitForClickAblitiy(WebElement element, int timeout){
         WebDriverWait wait = new WebDriverWait(WebDriverFactory.getDriver(), Duration.ofSeconds(timeout));
         return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
-    //       // Assert.assertTrue(DemoMRS.UserNameTextBox.isDisplayed());
+    public void selectByVisibleText(WebElement element,String text){
+        Select select = new Select(element);
+        select.selectByVisibleText(text);
+    }// its just fo String
+    //        uiHelpers.selectByVisibleText(faceBookPage.dayDropDownList, day);-- like this is coling from methods
+    public void selectByIndex(WebElement element,int index){
+        Select select = new Select(element);
+        select.selectByIndex(index);
+    } // It's just for Integer
+
 
 }
